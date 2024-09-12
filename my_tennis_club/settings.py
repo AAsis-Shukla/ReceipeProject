71a3b15a792640b2c3cd12cdf6edeedaf8e85a2a
AAsis-Shukla <169ashishkumar@gmail.com>
2024-09-12 16:11:31 +0530

    added vercel required files and code

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zmz+0%p!kn_wzp9x_7m87w$u2u)wk5%teyj(dtt7wf8-mrwp)j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -138,3 +138,7 @@ MEDIA_URL = '/media/'
 
 # Path where media is stored'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build","static")
